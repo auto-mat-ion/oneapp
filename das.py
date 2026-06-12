ef2554a71458cbb9482c71b8ece8697c11bdcd47
Derrick <derrickmulwa.programming@gmail.com>
2026-06-12 16:50:26 +0300

    Updated manual sender for test

--- das.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(
-    r"C:\Users\USER\Desktop\serious\freelance\oneapp\database\total andrew_8.1mln_bounces_cleaned.txt",
-    sep="jhjujhj",
-)
-
-df.iloc[7000000:7500000, :].to_csv("ffd.csv", index=False)
