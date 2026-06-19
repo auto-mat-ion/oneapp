963b5fa776167b4f9d152639fccc58264857e9ab
Derrick <derrickmulwa.programming@gmail.com>
2026-06-19 05:21:02 +0300

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bots.familybot import run_familybot, run_familybot_share
 from bots.hotmailbot import run_hotmailbot
 from bots.password_changer import run_password_changerbot
-from bots.email_sender_2 import main
+from bots.email_sender import main
 
 # from bots.email_sender_test import main_sec
 from bots.new_app_hotmail import run_second_app_hotmails
@@ -20,7 +20,7 @@ if (
 
 bot = int(
     input(
-        "Oneapp v1.41\nSelect bot to run:\n1. Familybot\n2. Hotmailbot\n3. Password Changer\n4. Email Sender\n5. Familybot Share\n6. New app Hotmailbot \n7. Save manual sender cookies\n8. Run manual bot\nEnter choice (1/2/3/4/5/6): "
+        "Oneapp v1.5\nSelect bot to run:\n1. Familybot\n2. Hotmailbot\n3. Password Changer\n4. Email Sender\n5. Familybot Share\n6. New app Hotmailbot \n7. Save manual sender cookies\n8. Run manual bot\nEnter choice (1/2/3/4/5/6): "
     )
 )
 
