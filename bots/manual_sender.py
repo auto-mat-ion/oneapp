91704d099bba6015358deb2db1a08f5aec68b9ef
Derrick <derrickmulwa.programming@gmail.com>
2026-06-22 21:29:20 +0300

    Manual sender

--- bots/manual_sender.py
+++ bots/manual_sender.py
@@ -3616,7 +3616,7 @@ class AccountManager:
         try:
             # offset = 150 limit 115 -> test in 3 hrs
 
-            offset = 0
+            offset = 80
             limit = 1000
             cursor = conn.cursor()
             query = (
