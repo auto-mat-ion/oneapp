2a26d9415035e8043317947ed4fe43ac9c4cae1d
Derrick <derrickmulwa.programming@gmail.com>
2026-07-02 23:36:38 +0300

    smtp stop update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if get_server_ip() == "test_ip":
 
 bot = int(
     input(
-        f"Oneapp v1.81\n\nSERVER IP: {get_server_ip()}\n\nSelect bot to run:\n1. Familybot\n2. Hotmailbot\n3. Password Changer\n4. Email Sender\n5. Familybot Share\n6. New app Hotmailbot \n7. Save manual sender cookies\n8. Run manual bot\nEnter choice (1/2/3/4/5/6): ".format()
+        f"Oneapp v1.82\n\nSERVER IP: {get_server_ip()}\n\nSelect bot to run:\n1. Familybot\n2. Hotmailbot\n3. Password Changer\n4. Email Sender\n5. Familybot Share\n6. New app Hotmailbot \n7. Save manual sender cookies\n8. Run manual bot\nEnter choice (1/2/3/4/5/6): ".format()
     )
 )
 
